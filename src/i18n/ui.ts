25e2e1403d94000ac0d9ed0a396f529100090864
Nano <gaomingzhao666@outlook.com>
2024-10-24 19:55:56 +0800

    feat: i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+	en: 'English',
+	ja: '日本語',
+	cn: '简体中文'
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+	en: {
+		'nav.home': 'Home',
+		'nav.about': 'About',
+		'nav.tags': 'Tags'
+	},
+	ja: {
+		'nav.home': 'ホーム',
+		'nav.about': 'アバウト',
+		'nav.tags': 'タッグ'
+	},
+	cn: {
+		'nav.home': '首页',
+		'nav.about': '关于',
+		'nav.tags': '标签'
+	}
+} as const
