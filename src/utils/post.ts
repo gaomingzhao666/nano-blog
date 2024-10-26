dae4db20af8b9142af1b90cba221394d2eb8dbdd
Nano <gaomingzhao666@outlook.com>
2024-10-26 14:00:22 +0800

    chore(remove): category and draft functionalities

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,16 +1,7 @@
 import { getCollection } from 'astro:content'
 
-export const getCategories = async () => {
-	const posts = await getCollection('blog')
-	const categories = new Set(
-		posts.filter((post) => !post.data.draft).map((post) => post.data.category)
-	)
-	return Array.from(categories)
-}
-
 export const getPosts = async (max?: number) => {
 	return (await getCollection('blog'))
-		.filter((post) => !post.data.draft)
 		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
 		.slice(0, max)
 }
@@ -19,7 +10,6 @@ export const getTags = async () => {
 	const posts = await getCollection('blog')
 	const tags = new Set()
 	posts
-		.filter((post) => !post.data.draft)
 		.forEach((post) => {
 			post.data.tags.forEach((tag) => {
 				tags.add(tag.toLowerCase())
@@ -33,15 +23,7 @@ export const getPostByTag = async (tag: string) => {
 	const posts = await getPosts()
 	const lowercaseTag = tag.toLowerCase()
 	return posts
-		.filter((post) => !post.data.draft)
 		.filter((post) => {
 			return post.data.tags.some((postTag) => postTag.toLowerCase() === lowercaseTag)
 		})
-}
-
-export const filterPostsByCategory = async (category: string) => {
-	const posts = await getPosts()
-	return posts
-		.filter((post) => !post.data.draft)
-		.filter((post) => post.data.category.toLowerCase() === category)
 }
