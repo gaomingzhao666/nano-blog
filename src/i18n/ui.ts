4cdb542339d5e5e143db1b76b7836f5145e78d42
Nano <gaomingzhao666@outlook.com>
2024-10-27 19:05:15 +0800

    chore(opt): modularize i18ns

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,25 +1,17 @@
+import en from './en.json'
+import ja from './ja.json'
+import cn from './cn.json'
+
 export const languages = {
-	en: 'English',
-	ja: '日本語',
-	cn: '简体中文'
+	'en': 'English',
+	'ja': '日本語',
+	'zh-CN': '简体中文'
 }
 
 export const defaultLang = 'en'
 
 export const ui = {
-	en: {
-		'nav.home': 'Home',
-		'nav.about': 'About',
-		'nav.tags': 'Tags'
-	},
-	ja: {
-		'nav.home': 'ホーム',
-		'nav.about': 'アバウト',
-		'nav.tags': 'タッグ'
-	},
-	cn: {
-		'nav.home': '首页',
-		'nav.about': '关于',
-		'nav.tags': '标签'
-	}
+	'en': en,
+	'ja': ja,
+	'zh-CN': cn
 } as const
