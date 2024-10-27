31abb56c255269f24c325c0a7a6aaeac04f34d7e
Nano <gaomingzhao666@outlook.com>
2024-10-27 18:49:48 +0800

    chore(compt): formatter

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import eslintPluginAstro from 'eslint-plugin-astro'
 
 export default [
-	...eslintPluginAstro.configs.recommended,
-	{
-		rules: {
-			'astro/no-set-html-directive': 'error'
-		}
-	}
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      'astro/no-set-html-directive': 'error',
+    },
+  },
 ]
