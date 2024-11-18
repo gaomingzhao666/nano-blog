8f7b962ec8751cff0a2757e29feb93eb7b23b1de
Nano <gaomingzhao666@outlook.com>
2024-11-18 14:24:26 +0800

    feat: set default i18n using by `localStorage`

--- src/utils/lang.ts
+++ src/utils/lang.ts
@@ -2,4 +2,10 @@ export const setLang = (lang: string) => {
   window.localStorage.setItem('lang', lang)
 }
 
-export const getLang = () => window.localStorage.getItem('lang') || 'en'
+export const getLang = (isReturnUrl: boolean) => {
+  if (isReturnUrl) {
+    return window.localStorage.getItem('lang') === 'en'
+      ? ''
+      : window.localStorage.getItem('lang') + '/'
+  } else return window.localStorage.getItem('lang') || 'en'
+}
