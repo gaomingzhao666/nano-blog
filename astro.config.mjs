df98f229533cc3466e9373c36b70d2cf2942f8a5
Nano <gaomingzhao666@outlook.com>
2024-12-22 10:51:39 +0800

    refactor: use absolute path `@` to replace relative path `./`

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import { remarkReadingTime } from './src/utils/readTime.ts'
+import { remarkReadingTime } from '@/utils'
 
 // https://astro.build/config
 export default defineConfig({
