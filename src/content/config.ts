111364aed4a41db90dd87dc76c4806e1fcd975a0
Nano <gaomingzhao666@outlook.com>
2025-01-06 12:45:25 +0800

    chore: limit the `title` max string length to 60

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,16 @@
 import { defineCollection, z } from 'astro:content'
 
+// 60 characters
+// With Google's Title update, shorter titles are preferred.
+// If the title is too long, they are more likely to be rewritten.
+// Use these rules of thumb when it comes to the title tag
+// length: Maximum length: 60 characters or 575 pixels.2
+
 const blog = defineCollection({
   // Type-check frontmatter using a schema
   schema: ({ image }) =>
     z.object({
-      title: z.string().max(80),
+      title: z.string().max(60),
       description: z.string(),
       // Transform string to Date object
       pubDate: z
