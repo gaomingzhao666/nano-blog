b71ab265c6b36b06771f07d8f512733478e80bd8
Nano <gaomingzhao666@outlook.com>
2025-01-10 20:01:16 +0800

    chore: change code theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime],
     shikiConfig: {
-      theme: 'dracula',
+      theme: 'github-dark',
       wrap: true,
     },
   },
