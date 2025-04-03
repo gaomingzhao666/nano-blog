d7368f637e1e632c9417bbbf5a6e108c28370e62
Nano <gaomingzhao666@outlook.com>
2025-04-03 19:07:09 +0900

    chore: remove unused importation

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,8 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
 import pagefind from 'astro-pagefind'
 import { remarkReadingTime } from './src/utils/readTime'
-
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
